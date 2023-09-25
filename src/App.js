c8b27e7b946894e49218dffc947bc58b49580d46
kalaiselvi2023 <kalaiselvip633@gmail.com>
2023-09-25 14:41:23 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Navbar from "./Component/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+export default App;
